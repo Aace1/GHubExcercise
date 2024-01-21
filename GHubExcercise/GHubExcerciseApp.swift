62fa9609cf8cda785d77c6cfa59244185148b8c9
Aaron Anderson <aace2@me.com>
2024-01-21 13:21:39 -0500

    Initial Commit

---
+++ GHubExcercise/GHubExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHubExcerciseApp.swift
+//  GHubExcercise
+//
+//  Created by TheForce on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GHubExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
